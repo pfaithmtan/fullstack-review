0207f3f9cb21cbdb735cbcc0e72bc73835c458e4
Faith <pfaithmtan@gmail.com>
2019-10-05 00:58:12 -0700

    Create POST request on client side

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,26 +3,33 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
   }
 
-  search (term) {
-    console.log(`${term} was searched`);
+  search(term) {
     // TODO
+    axios.post('/repos', { username: term })
+      .then(function (response) {
+        console.log(`${term} was searched`);
+      })
+      .catch(function (error) {
+        console.log(`error: ${error}`);
+      })
   }
 
-  render () {
+  render() {
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos} />
+      <Search onSearch={this.search.bind(this)} />
     </div>)
   }
 }
